32cf2efb2dda721c3a219c363005304b29a0f24e
wantuh <48893802+NucLuc@users.noreply.github.com>
2024-02-24 01:36:18 +0000

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -336,7 +336,7 @@ class Utility(commands.Cog):
         embed.add_field(name="Latency", value=f"{self.bot.latency * 1000:.2f} ms")
         embed.add_field(name="Version", value=f"`{self.bot.version}`")
         embed.add_field(name="Authors", value="`kyb3r`, `Taki`, `fourjr`")
-        embed.add_field(name="Hosting Method", value="`WANTUH` \nContact <@561312593155981323>")
+        embed.add_field(name="Hosting Method", value="`WANTUH`")
 
         changelog = await Changelog.from_url(self.bot)
         latest = changelog.latest_version
